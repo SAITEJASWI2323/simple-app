127c971be6d83193e8c39ac616a59fad5d269c29
unknown <Administrator@SJT219SITE0019.VITUNIVERSITY.LOCAL>
2026-03-26 16:56:56 +0530

    first commit

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,4 @@
-FROM openjdk:17-jdk-slim
+FROM eclipse-temurin:17-jdk
 WORKDIR /app
-# Make sure your Maven build actually names the jar 'simple-java-app-1.0.jar'
 COPY target/simple-java-app-1.0.jar app.jar
-EXPOSE 8086
-ENTRYPOINT ["java", "-jar", "app.jar"]
+CMD ["java", "-jar", "app.jar"]
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,12 @@
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Java CI/CD Application Started...");
+        
+        // This loop is CRITICAL to keep the container alive in Kubernetes
+        while (true) {
+            Thread.sleep(5000);
+            System.out.println("Heartbeat: App is still running...");
+        }
     }
-    
 }
+
